01f23aef5008cad60661214b5633bed5b88823b7
Insidioused <66269378+Insidioused@users.noreply.github.com>
2024-07-04 12:35:33 +0000

    addition is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,58 @@
-function rungame(){
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        });
+    }
+
+    runGame("addition");
+});
+
+function runGame(gameType) {
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if(gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type ${gameType}. Aborting`;
+    }
+}
+
+function checkAnswer() {
 
 }
 
-function checkAnswer(){
+function calculateCorrectAnswer() {
 
 }
 
-function calculateCorrectAnswer(){
+function incrementScore() {
 
 }
 
-function incrementAnsers(){
+function incrementWrongAnswer() {
 
 }
 
-function incrementWrongAnswer(){
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById(`operand1`).textContent = operand1;
+    document.getElementById(`operand2`).textContent = operand2;
+    document.getElementById(`operator`).textContent = "+";
+}
+
+function displaySubtractQuestion() {
 
 }
 
-function displayAdditionQuestion(){
-
-}
-
-function displaySubtractQuestion(){
-
-
-}
-
-function displayMultiplyQuestion(){
-
+function displayMultiplyQuestion() {
+    
 }
