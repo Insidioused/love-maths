a3d7dd23391c7712242f709f7375fb3d66ec7de6
Insidioused <66269378+Insidioused@users.noreply.github.com>
2024-07-04 11:45:48 +0000

    js basic structor done

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+function rungame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementAnsers(){
+
+}
+
+function incrementWrongAnswer(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+
+
+}
+
+function displayMultiplyQuestion(){
+
+}
